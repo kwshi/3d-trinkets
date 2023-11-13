43dd2d3adb02eebb6ae756a62a3c6484d2bf446c
Kye Shi <kiwishi@pm.me>
2023-11-12 16:51:15 -0800

    script/solvespace: also output stl for original source

--- script/solvespace/__main__.py
+++ script/solvespace/__main__.py
@@ -10,27 +10,27 @@ from . import config
 from .slvs import sketch as slvs_sketch
 
 parser = argparse.ArgumentParser()
-parser.add_argument('config_dir', type=str)
+parser.add_argument("config_dir", type=str)
 args = parser.parse_args()
 
 config_dir = typing.cast(str, args.config_dir)
 assert os.path.isdir(config_dir)
 
-with open(os.path.join(config_dir, 'solvespace.toml'), 'rb') as config_file:
+with open(os.path.join(config_dir, "solvespace.toml"), "rb") as config_file:
     config = config.Config.parse(tomllib.load(config_file))
-    
+
 source_path = os.path.join(config_dir, config.source)
-assert source_path.endswith('.slvs')
-source_name = source_path.removesuffix('.slvs')
-with open(source_path, 'rb') as f:
+assert source_path.endswith(".slvs")
+source_name = source_path.removesuffix(".slvs")
+with open(source_path, "rb") as f:
     sketch_base = slvs_sketch.Sketch.parse(f)
 
 config.validate(sketch_base)
 out_paths: list[str] = []
 for name, sketch in config.apply(sketch_base).items():
-    out_path = f'{source_name}.{name}.slvs'
+    out_path = f"{source_name}.{name}.slvs"
     out_paths.append(out_path)
-    with open(out_path, 'wb') as out:
+    with open(out_path, "wb") as out:
         sketch.serialize(out)
 
-sp.call(['solvespace-cli', 'export-mesh', '--output', '%.stl', *out_paths])
+sp.call(["solvespace-cli", "export-mesh", "--output", "%.stl", source_path, *out_paths])
